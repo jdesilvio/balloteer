77c2c2dfbb87813a8a45941f1c686176d8021bdb
John DeSilvio <john.desilvio@yahoo.com>
2016-07-22 22:35:33 -0400

    Added functions for Balloteer

--- lib/balloteer.ex
+++ lib/balloteer.ex
@@ -1,19 +1,38 @@
 defmodule Balloteer do
   use Application
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
     children = [
-      # Define workers and child supervisors to be supervised
-      # worker(Balloteer.Worker, [arg1, arg2, arg3]),
+      worker(Balloteer.Booth, [])
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Balloteer.Supervisor]
+    opts = [strategy: :simple_one_for_one, name: Balloteer.Supervisor]
     Supervisor.start_link(children, opts)
   end
+
+  @doc """
+  Create a supervised voting booth for a given `task_id`.
+  """
+  def create_booth(task_id) do
+    Supervisor.start_child(Balloteer.Supervisor, [task_id])
+  end
+
+  @doc """
+  Return a list of all PIDs under the Supervisor
+  """
+  def get_pids() do
+    pop_pid(Supervisor.which_children(Balloteer.Supervisor), [])
+  end
+
+  defp pop_pid([head|tail], list) do
+    {_, pid, _, _} = head
+    pop_pid(tail, [pid|list])
+  end
+
+  defp pop_pid([], list) do
+    list
+  end
 end
+
